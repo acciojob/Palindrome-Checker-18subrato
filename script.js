7cd9bdba35bcf5cb1268946f5000392eec5055bd
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-04-05 18:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+	str = str.toLowerCase().replace(/\W/g,'')
+    let newStr = str.split('').reverse().join('')
+    console.log(newStr)
+    console.log(str)
+    if(newStr == str){
+        return true;
+    }
+    
+    return false;
 }
 module.exports = palindrome
